ff0a5ea9d4d6bc0b8fde66d12cec1a7c3da9a9d0
Hadrien Boutteville <h.boutteville@leosidea.com>
2022-11-14 01:57:34 +0100

    feat: close modal
    Issue #1

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,14 +10,20 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalCloseBtn = document.querySelector(".close");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+modalCloseBtn.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+function closeModal() {
+  modalbg.style.display = "none";
+}
 
